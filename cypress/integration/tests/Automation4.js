ec08d69c851b96145b7b4bfb579bbffc303d1502
Ayo Adesokan <adesokanayo@gmail.com>
2022-04-30 09:29:51 -0600

    Solution to assignment

--- cypress/integration/tests/Automation4.js
+++ cypress/integration/tests/Automation4.js
@@ -5,8 +5,12 @@ describe ("Automation 4" , () => {
     cy.get("input[id=l1]").type("Tester");
     cy.get("input[id=f2]").type("Matt");
     cy.get("input[id=l2]").type("Tester");
-    cy.get('[type="radio2-m"]').check("male");
-    cy.get("input[type=submit2]").click()
+    cy.get("input[id=radio1-f]").check("female");
+    cy.get("input[id=submit1]").click()
+    
+
+
+
     
     });
     })
--- cypress/integration/tests/Automation5.js
+++ cypress/integration/tests/Automation5.js
@@ -1,11 +1,12 @@
 describe ("Automation 5" , () => {
     it("Sprint 5"  , () => {
     cy.visit("https://ultimateqa.com/sample-application-lifecycle-sprint-5/");
+    cy.get('[id="radio1-0"]').check("other");
     cy.get("input[id=f1]").type("Damilola");
     cy.get("input[id=l1]").type("Software");
     cy.get("input[id=f2]").type("Matthew");
     cy.get("input[id=l2]").type("DTester");
-    cy.get('[type="radio2-0"]').check("other");
+    cy.get('[id="radio2-0"]').check("other");
 
     cy.get("input[id=submit2]").click();
 
